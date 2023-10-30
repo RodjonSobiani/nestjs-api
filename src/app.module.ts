f33e001c2a05dcce14092c2983541f3c605e62f7
rodjonsobiani <srripstomsk@gmail.com>
2023-10-30 17:12:51 +0700

    Created DB && config files.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 // import { AppController } from './_old/app.controller';
 // import { AppService } from './_old/app.service';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
   // controllers: [AppController],
   // providers: [AppService],
 })
