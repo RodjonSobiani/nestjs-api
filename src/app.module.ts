fd04d216620dedea8a634826b1f7883e0fe28d9b
rodjonsobiani <srripstomsk@gmail.com>
2023-10-30 14:56:34 +0700

    Init commit.

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+// import { AppController } from './_old/app.controller';
+// import { AppService } from './_old/app.service';
+
+@Module({
+  imports: [],
+  // controllers: [AppController],
+  // providers: [AppService],
+})
+export class AppModule {}
